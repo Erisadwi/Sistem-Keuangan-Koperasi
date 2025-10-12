4f7db690904a9b5dbe959b4d81d22e325b3d0d0a
dewiuuuu <nugrainidewi@gmail.com>
2025-10-13 05:43:59 +0700

    tambah route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/', function () {
 Route::get('/anggota/beranda', function () {
     return view('anggota.beranda');
 })->name('anggota.beranda');
+
+Route::get('/anggota/laporan-simpanan', function () {
+    return view('anggota.laporan-simpanan');
+})->name('anggota.laporan-simpanan');
