9b3956a0390cc5e6144f261c0bdcf743a5e7e81a
Erisadwi <erisadwi099@gmail.com>
2025-10-13 12:50:26 +0700

    perbaikan tabel data pengajuan

--- resources/views/anggota/data-pengajuan.blade.php
+++ resources/views/anggota/data-pengajuan.blade.php
@@ -11,53 +11,63 @@
 
 @section('content')
 
-{{-- contoh di mana saja --}}
 <x-menu.date-filter/>
 
-
-<div class="shu-table-wrap">
-  <table class="shu-table">
+<div class="pengajuan-table-wrap">
+  <table class="pengajuan-table">
     <thead>
-      <tr class="head-title">
-        <th colspan="4">Pembagian SHU</th>
-      </tr>
       <tr class="head-group">
-        <th colspan="2" class="group-left">Jasa Usaha</th>
-        <th colspan="2" class="group-right">Modal Usaha</th>
+        <th>Tanggal</th>
+        <th>Jenis</th>
+        <th>Nominal</th>
+        <th>Jml. Angsuran</th>
+        <th>Ket</th>
+        <th>Tanggal Update</th>
+        <th>Status</th>
       </tr>
     </thead>
 
     <tbody>
-      <tr>
-        <td class="label">Laba Anggota</td>
-        <td class="value">875.000 {{-- -{{ number_format($shu->laba_anggota ?? 0, 0, ',', '.') }} --}}</td>
-        <td class="label">Simpanan Anggota</td>
-        <td class="value">5.210.000 {{-- -{{ number_format($shu->saldo_simpanan ?? 0, 0, ',', '.') }} --}}</td>
-      </tr>
+      @forelse(($pengajuan ?? collect()) as $idx => $row)
+        <tr>
+          <td>
+            {{-- Jika paginate, nomor urut mengikuti halaman --}}
+            @if($pengajuan instanceof \Illuminate\Pagination\AbstractPaginator)
+              {{ ($pengajuan->currentPage() - 1) * $pengajuan->perPage() + $loop->iteration }}
+            @else
+              {{ $idx + 1 }}
+            @endif
+          </td>
 
-      <tr>
-        <td class="label">Total Laba</td>
-        <td class="value">59.547.035{{-- -{{ number_format($shu->total_laba ?? 0, 0, ',', '.') }} --}}</td>
-        <td class="label">Total Simpanan</td>
-        <td class="value">985.197.260{{-- -{{ number_format($shu->total_simpanan ?? 0, 0, ',', '.') }} --}}</td>
-      </tr>
+          <td>{{ $row->kode ?? '' }}</td>
+          <td>{{ $row->tanggal ?? '' }}</td>
+          <td>{{ $row->jenis ?? '' }}</td>
 
-      <tr>
-        <td class="label">Jasa Usaha</td>
-        <td class="value negative">-2.706.954{{-- {{ number_format($shu->jasa_usaha ?? 0, 0, ',', '.') }} --}}</td>
-        <td class="label">Jasa Simpanan</td>
-        <td class="value negative">-1.160.123 {{-- -{{ number_format($shu->jasa_simpanan ?? 0, 0, ',', '.') }} --}}</td>
-      </tr>
+          <td>
+            @php $nom = $row->nominal ?? null; @endphp
+            {{ $nom !== null ? number_format($nom, 0, ',', '.') : '' }}
+          </td>
 
-      <tr>
-        <td class="label">SHU Jasa Usaha</td>
-        <td class="value negative">-39.777 {{-- -{{ number_format($shu->shu_jasa_usaha ?? 0, 0, ',', '.') }} --}}</td>
-        <td class="label">SHU Jasa Modal</td>
-        <td class="value negative">-6.135 {{-- -{{ number_format($shu->shu_jasa_modal ?? 0, 0, ',', '.') }} --}}</td>
-      </tr>
+          <td>
+            @php
+              $status = trim((string)($row->status ?? ''));
+              $cls = '';
+              if ($status === 'Disetujui') $cls = 'disetujui';
+              elseif ($status === 'Ditolak') $cls = 'ditolak';
+              elseif ($status === 'Menunggu') $cls = 'menunggu';
+            @endphp
+            <span class="badge {{ $cls }}">{{ $status }}</span>
+          </td>
+
+      @empty
+        <tr>
+          <td colspan="7" class="empty-cell">Belum ada data pengajuan.</td>
+        </tr>
+      @endforelse
     </tbody>
   </table>
 </div>
+
 <x-menu.pagination/>
 
 @endsection
