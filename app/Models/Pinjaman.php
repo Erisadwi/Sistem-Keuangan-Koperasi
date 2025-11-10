e7c877e0fe0d1e821954956f335886ca7c28e4c6
Divia Astrina <143948767+diviastrina@users.noreply.github.com>
2025-11-10 10:59:07 +0700

    revisi models pinjaman

--- app/Models/Pinjaman.php
+++ app/Models/Pinjaman.php
@@ -83,9 +83,4 @@ class Pinjaman extends Model
         return $this->belongsTo(JenisAkunTransaksi::class, 'id_jenisAkunTransaksi_sumber', 'id_jenisAkunTransaksi');
     }
 
-    public function angsuran()
-    {
-    return $this->hasMany(\App\Models\Angsuran::class, 'id_pinjaman', 'id_pinjaman');
-    }
-
 }
