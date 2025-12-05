e37e212594f618f6b4a7ac9766c529d492920d72
Divia Astrina <143948767+diviastrina@users.noreply.github.com>
2025-12-05 13:43:25 +0700

    api transfer

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MasterData\AnggotaController;
+use App\Http\Controllers\Admin\TransaksiKas\TransaksiTransferController;
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'API is working']);
@@ -10,3 +11,7 @@ Route::get('/ping', function () {
 Route::get('/anggota', [AnggotaController::class, 'apiIndex']);
 Route::post('/anggota', [AnggotaController::class, 'apiStore']);
 
+Route::get('/transfer', [TransaksiTransferController::class, 'apiIndex']);     
+Route::post('/transfer', [TransaksiTransferController::class, 'apiStore']);    
+Route::put('/transfer/{id}', [TransaksiTransferController::class, 'apiUpdate']); 
+Route::delete('/transfer/{id}', [TransaksiTransferController::class, 'apiDestroy']); 
