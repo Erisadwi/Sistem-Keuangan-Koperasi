93392ee295e04a74373277a3dc1b1b707d120d27
Erisadwi <erisadwi099@gmail.com>
2025-12-05 14:46:03 +0700

    test pemasukan

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,42 +3,26 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'id_user' => Str::uuid(),
+            'nama_lengkap' => $this->faker->name(),
+            'alamat_user' => $this->faker->address(),
+            'telepon' => $this->faker->phoneNumber(),
+            'username' => $this->faker->unique()->userName(),
+            'password' => Hash::make('password'),
+            'foto_user' => null,
+            'jenis_kelamin' => 'L',
+            'status' => 'aktif',
+            'tanggal_masuk' => now(),
+            'tanggal_keluar' => null,
+            'id_role' => null,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
