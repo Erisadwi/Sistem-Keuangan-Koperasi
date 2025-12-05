0fd8f523b1a2dbfbcaba67c6068d67a4934da166
Divia Astrina <143948767+diviastrina@users.noreply.github.com>
2025-12-05 13:56:36 +0700

    api penarikan tunai

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MasterData\AnggotaController;
 use App\Http\Controllers\Admin\TransaksiKas\TransaksiTransferController;
+use App\Http\Controllers\Admin\Simpanan\PenarikanTunaiController;
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'API is working']);
@@ -15,3 +16,8 @@ Route::get('/transfer', [TransaksiTransferController::class, 'apiIndex']);
 Route::post('/transfer', [TransaksiTransferController::class, 'apiStore']);    
 Route::put('/transfer/{id}', [TransaksiTransferController::class, 'apiUpdate']); 
 Route::delete('/transfer/{id}', [TransaksiTransferController::class, 'apiDestroy']); 
+
+Route::get('/penarikan-tunai', [PenarikanTunaiController::class, 'apiIndex']);
+Route::post('/penarikan-tunai', [PenarikanTunaiController::class, 'apiStore']);
+Route::put('/penarikan-tunai/{id}', [PenarikanTunaiController::class, 'apiUpdate']);
+Route::delete('/penarikan-tunai/{id}', [PenarikanTunaiController::class, 'apiDestroy']);
