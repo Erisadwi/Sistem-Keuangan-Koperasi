8a50aec2fdb333dd9a0858689c55cd39322f3b7c
dewiuuuu <nugrainidewi@gmail.com>
2025-12-06 08:18:08 +0700

    laporan simpanan api

--- app/Http/Controllers/Anggota/LaporanSimpananController.php
+++ app/Http/Controllers/Anggota/LaporanSimpananController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Anggota;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Simpanan;
+use Illuminate\Http\Request;  
 
 class LaporanSimpananController extends Controller
 {
@@ -34,4 +35,53 @@ class LaporanSimpananController extends Controller
 
         return view('anggota.laporan-simpanan', compact('data', 'anggota'));
     }
+
+    public function apiIndex(Request $request)
+    {
+        $username = $request->query('username_anggota');
+
+        if (!$username) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'username_anggota wajib dikirim'
+            ], 400);
+        }
+
+        $anggota = \App\Models\Anggota::where('username_anggota', $username)->first();
+
+        if (!$anggota) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Anggota tidak ditemukan'
+            ], 404);
+        }
+
+        $data = \App\Models\Simpanan::where('id_anggota', $anggota->id_anggota)
+            ->orderBy('tanggal_transaksi', 'asc')
+            ->paginate(10);
+
+        $data->getCollection()->transform(function ($item) {
+
+            if (str_starts_with($item->kode_simpanan, 'TRD')) {
+                $item->jenis = 'Setoran';
+            } elseif (str_starts_with($item->kode_simpanan, 'TRK')) {
+                $item->jenis = 'Penarikan';
+            } else {
+                $item->jenis = '-';
+            }
+
+            $item->tanggal = $item->tanggal_transaksi;
+            $item->jumlah = $item->jumlah_simpanan;
+
+            return $item;
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'username_anggota' => $username,
+            'total' => $data->total(),
+            'data' => $data->items()
+        ]);
+    }
+
 }
